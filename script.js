1050134e85be35bfbc822871759957660a0488f3
Diego Croquer <diego.croquer@gm2dev.com>
2025-08-26 12:44:28 -0300

    primer ejercicio

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hola");
+//Crea un bloque de código que compare dos variables numéricas. Si el número A es mayor que el número B, imprimir en consola ‘El número A es mayor a B‘.
+//
+let numeroA = 5;
+let numeroB = 9;
+if (numeroA > numeroB){
+    console.log("El numero A es mayor a B")
+}
+else{
+    console.log("no")
+}
