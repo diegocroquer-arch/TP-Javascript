4bdee11c87d02fb03fb031e2a0b009807fcef5b1
Diego Croquer <diego.croquer@gm2dev.com>
2025-08-27 11:21:04 -0300

    inicio de arrays

--- script.js
+++ script.js
@@ -1,67 +1,84 @@
-console.log("hola");
-//Crea un bloque de código que compare dos variables numéricas. Si el número A es mayor que el número B, imprimir en consola ‘El número A es mayor a B‘.
-//
-let numeroA = 5;
-let numeroB = 9;
-if (numeroA > numeroB) {
-  console.log("El numero A es mayor a B");
-} else {
-  console.log("no");
-}
-//Crea un bloque de código que tome la edad de un usuario y la compare con un número de referencia.
-//  Si la edad es mayor al número de referencia, imprimir en consola un mensaje.//
+// console.log("hola");
+// //Crea un bloque de código que compare dos variables numéricas. Si el número A es mayor que el número B, imprimir en consola ‘El número A es mayor a B‘.
+// //
+// let numeroA = 5;
+// let numeroB = 9;
+// if (numeroA > numeroB) {
+//   console.log("El numero A es mayor a B");
+// } else {
+//   console.log("no");
+// }
+// //Crea un bloque de código que tome la edad de un usuario y la compare con un número de referencia.
+// //  Si la edad es mayor al número de referencia, imprimir en consola un mensaje.//
 
-let edad = 18
-let ref = 11
-if (edad > ref){
-    console.log(`La edad es mayor que el numero de referencia ${ref}`)
-}
+// let edad = 18
+// let ref = 11
+// if (edad > ref){
+//     console.log(`La edad es mayor que el numero de referencia ${ref}`)
+// }
 
-//Crea un bloque de código que compare dos números e imprima en consola un valor booleano.
-//
+// //Crea un bloque de código que compare dos números e imprima en consola un valor booleano.
+// //
 
-let saldo = 5000
-let deuda = 2000
+// let saldo = 5000
+// let deuda = 2000
 
-if (saldo > deuda) {
-  console.log (true)
-}
+// if (saldo > deuda) {
+//   console.log (true)
+// }
 
-//Crea un bloque de código que compare dos nombres, y si ambos son iguales imprima en consola el nombre.//
+// //Crea un bloque de código que compare dos nombres, y si ambos son iguales imprima en consola el nombre.//
 
-let nombre1 = "Diego"
-let nombre2 = "Diego"
-if(nombre1 == nombre2){
-  console.log (nombre1)
-}
+// let nombre1 = "Diego"
+// let nombre2 = "Diego"
+// if(nombre1 == nombre2){
+//   console.log (nombre1)
+// }
 
-//Crea un bloque de código que imprima en consola un número en caso de que sea un número negativo.
-let numeros = -3
-if (numeros < 0){
-  console.log(numeros)
-}
+// //Crea un bloque de código que imprima en consola un número en caso de que sea un número negativo.
+// let numeros = -3
+// if (numeros < 0){
+//   console.log(numeros)
+// }
 
-//Crea un bloque de código que compare dos números. Si el número es par, imprimir en consola ‘El número es par’. 
-// De lo contrario, imprimir ‘El número es impar’.
-let numP = 8
-let numIP = 5
-if ( (numP + numIP) %2 == 0){
-  console.log ("el numero es par")
-}
-else {
-  console.log("el numero es impar")
-}
+// //Crea un bloque de código que compare dos números. Si el número es par, imprimir en consola ‘El número es par’. 
+// // De lo contrario, imprimir ‘El número es impar’.
+// let numP = 8
+// let numIP = 5
+// if ( (numP + numIP) %2 == 0){
+//   console.log ("el numero es par")
+// }
+// else {
+//   console.log("el numero es impar")
+// }
 
 //Crea una función que tome de referencia una variable string cuyo valor sea una letra,
 //  y luego imprima en consola si es una vocal o una consonante.
-ref = "A"
+// ref = "A"
 
-if(ref == "a" ||ref =="e" ||ref =="i"||ref== "o"||ref=="u" ){
-  console.log ("Es una vocal")
+// if(ref == "a" ||ref =="e" ||ref =="i"||ref== "o"||ref=="u" ){
+//   console.log ("Es una vocal")
+// }
+// if(ref == "A" ||ref =="E" ||ref =="I"||ref== "O"||ref=="U" ){
+//   console.log ("Es una vocal")
+// }
+// else {
+//   console.log("Es una consonante")
+// }
+//Crea un bloque de código que tome de referencia un número 
+// e imprima en pantalla si el número es menor a 10, mayor a 50, o si está entre esos dos valores.
+ref = 60
+if (ref< 10){
+  console.log("la referencia es menor a 10")
 }
-if(ref == "A" ||ref =="E" ||ref =="I"||ref== "O"||ref=="U" ){
-  console.log ("Es una vocal")
+else if(ref>=10){
+  console.log ("La referencia esta entre 10 y 50")
+}
+else if(ref<=50){
+  console.log ("La referencia esta entre 10 y 50")
 }
 else {
-  console.log("Es una consonante")
+  console.log("La referencia es mayor a 50")
 }
+
+//arrays/
