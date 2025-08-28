42fb12d5cac27163bb0df721017e500b6e93a581
Diego Croquer <diego.croquer@gm2dev.com>
2025-08-28 12:38:34 -0300

    consignas agregadas

---
+++ funciones.js
@@ -0,0 +1,29 @@
+//Ejercicios de Funciones en JavaScript
+
+//1. Crea una función llamada 'saludar' que muestre en consola el mensaje: Hola, bienvenido a
+//JavaScript.
+
+//2. Escribe una función llamada 'sumar' que reciba dos números y muestre el resultado en consola.
+
+//3. Haz una función llamada 'esMayor' que reciba dos números y muestre true si el primero es
+//mayor que el segundo.
+
+//4. Crea una función llamada 'esPar' que reciba un número y muestre true si es par o false si es
+//impar.
+
+//5. Escribe una función llamada 'multiplicar' que reciba tres números y muestre el resultado en
+//consola.
+
+//6. Haz una función llamada 'compararStrings' que reciba dos textos y muestre true si son iguales o
+//false si son diferentes.
+
+//7. Crea una función llamada 'areaRectangulo' que reciba base y altura, y muestre el área en
+//consola.
+
+//8. Escribe una función llamada 'mayorDeEdad' que reciba una edad y muestre 'Es mayor de edad'
+//si es mayor o igual a 18, y 'Es menor de edad' si no lo es.
+
+//9. Crea una función llamada 'promedio' que reciba tres números y muestre el promedio en consola.
+
+//10. Escribe una función llamada 'calcularIVA' que reciba un precio y muestre el precio final con un
+//21% de IVA incluido.//
